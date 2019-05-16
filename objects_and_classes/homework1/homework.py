cd795c4f30843b349381e03542a847204ea06218
Valeriia Mi <vera1996111@gmail.com>
2019-05-16 21:18:55 +0300

    Code refactor

--- objects_and_classes/homework1/homework.py
+++ objects_and_classes/homework1/homework.py
@@ -50,38 +50,72 @@
 import uuid
 import random
 from homework1.constants import *
+
+
 class Car():
 
-    def __init__(self, price: float,type,producer, milege:float,):
+    def __init__(self, price: float, type, producer, milege: float, ):
         self.price = price
         if type in CARS_TYPES:
             self.type = type
         if producer in CARS_PRODUCER:
-            self.producer= producer
+            self.producer = producer
         self.number = uuid.uuid4()
         self.milege = milege
 
-
-
-    def compair_cars_by_price(self,other):
+    def compair_cars_by_price(self, other):
         if self.price == other.price:
             return "{} and {} cars have same price".format(self.type and other.type)
         elif self.price > other.price:
-            return "{} more expensive that {}".format(self.type,other.type)
+            return "{} more expensive that {}".format(self.type, other.type)
         else:
-            return "{} less expencive that {}".format(self.type,other.type)
+            return "{} less expencive that {}".format(self.type, other.type)
 
-    def __str__(self):
-        return "Car(My price: %r, my type: %r, my producer: %r, my number:%r, my milege: %r)" % (self.price, self.type, self.producer, self.number, self.milege)
-
-    def __repr__(self):
-        return "Car(My price: %r, my type: %r, my producer: %r, my number:%r, my milege: %r)" % (self.price, self.type, self.producer,self.number,self.milege)
 
     def change_number(self):
         self.number = str(uuid.uuid4())
         return "You new number is {}".format(self.number)
+    def __str__(self):
+        return "Car(My price: %r, my type: %r, my producer: %r, my number:%r, my milege: %r)" % (
+        self.price, self.type, self.producer, self.number, self.milege)
 
-    '''def __iter__(self):
+    def __repr__(self):
+        return "Car(My price: %r, my type: %r, my producer: %r, my number:%r, my milege: %r)" % (
+        self.price, self.type, self.producer, self.number, self.milege)
+
+
+
+
+
+class Garage():
+    def __init__(self, cars: list, town, places: int, owner=None):
+        if town in TOWNS:
+            self.town = town
+        self.cars = cars
+        self.places = places - len(cars)
+        self.owner = uuid.uuid4()
+        self.counter = 0
+
+    def add(self, new_car):
+        if self.places != 0:
+            self.cars.append([new_car])
+            self.places -= 1
+            return 'Your car has been added'
+        else:
+            return 'No more place'
+
+    def remove(self, remove_car):
+        self.cars.remove(remove_car)
+        self.places += 1
+        return 'Your car was removed'
+
+    def hit_hap(self):
+        all_price = 0
+        for this_car in self.cars:
+            all_price += this_car.__dict__['price']
+        return "This is price your all car {}".format(all_price)
+
+    def __iter__(self):
         return self
 
     def __next__(self):
@@ -90,63 +124,22 @@ class Car():
             self.counter += 1
             return my_car
         else:
-            raise StopIteration'''
-class Garage():
-     def __init__(self,cars: list,town,places:int,owner = None):
-        if town in TOWNS:
-            self.town = town
-        self.cars = cars
-        self.places = places - len(cars)
-        self.owner = uuid.uuid4()
-        self.counter = 0
+            raise StopIteration
 
-     def add (self,new_car):
-        if self.places != 0:
-            self.cars += [new_car]
-            self.places -= 1
-            return 'Your car has been added'
-        else:
-            return 'No more place'
-
-     def remove(self,remove_car):
-         self.cars.remove(remove_car)
-         self.places += 1
-         return 'Your car was removed'
-
-     def hit_hap(self):
-        all_price = 0
-        for this_car in self.cars:
-            all_price += this_car.__dict__['price']
-        return "This is price your all car {}".format(all_price)
-
-
-
-     def __iter__(self):
-         return self
-
-     def __next__(self):
-         if self.counter< len(self.cars):
-             my_car = self.cars[self.counter]
-             self.counter+=1
-             return my_car
-         else:
-             raise StopIteration
-
-
-     def __str__(self):
+    def __str__(self):
         return "Garage(My town:%r, my cars: %r, i have %r places)" % (self.town, self.cars, self.places)
 
-
-     def __repr__(self):
+    def __repr__(self):
         return "Garage(My town:%r, my cars: %r, i have %r places)" % (self.town, self.cars, self.places)
+
 
 class Collectioner():
 
-    def __init__(self,name:str,garages=0):
+    def __init__(self, name: str, garages=0):
         self.name = name
         self.garages = [garages]
         self.register_id = uuid.uuid4()
-        self.counter =0
+        self.counter = 0
 
     def hit_hat(self):
         price_collectioner = 0
@@ -155,18 +148,44 @@ class Collectioner():
                 price_collectioner += this_car.__dict__['price']
             return price_collectioner
 
-
-
     def garages_count(self):
         return "{} - is len my garages".format(len(self.garages))
 
     def cars_count(self):
-        count= 0
+        count = 0
         for members in self.garages:
             for some in members:
-                count+=1
+                count += 1
         return "{} - is how many cars i have".format(count)
 
+
+
+    def add_car(self, new_car, garage=None):
+        for garage_class in self.garages:
+            if garage in self.garages:
+                if garage.__dict__['places'] != 0:
+                    garage.__dict__['cars'] += [new_car]
+                    return 'Car was added'''
+                else:
+                    return 'No more place in garage.Choose next one'''
+
+            else:
+                a = 0
+                if a < int(garage_class.__dict__['places']):
+                    a = int(garage_class.__dict__['places'])
+                if a == int(garage_class.__dict__['places']) and a != 0:
+                    garage_class.__dict__['cars'] += [new_car]
+                    return 'Car was added'
+                else:
+                    return 'No more place'
+
+    def compare_collectioner(self, other):
+        if self.hit_hat() > other.hit_hat():
+            return "{} have more expencive car than {}".format(self.name, other.name)
+        elif self.hit_hat() < other.hit_hat():
+            return "{} have less expencive car than {}".format(self.name, other.name)
+        else:
+            return "{} and {} have same cars".format(self.name, other.name)
     def __iter__(self):
         return self
 
@@ -178,42 +197,16 @@ class Collectioner():
         else:
             raise StopIteration
 
-    def add_car(self,new_car,garage=None):
-        for garage_class in self.garages:
-            if garage in self.garages:
-                if garage.__dict__['places'] != 0:
-                    garage.__dict__['cars'] += [new_car]
-                    return 'Car was added'''
-                else:
-                    return 'No more place in garage.Choose next one'''
-
-            else:
-                a = 0
-                if a<int(garage_class.__dict__['places']):
-                    a = int(garage_class.__dict__['places'])
-                if a == int(garage_class.__dict__['places']) and a!=0:
-                    garage_class.__dict__['cars'] += [new_car]
-                    return 'Car was added'
-                else:
-                    return 'No more place'
-
-    def compare_collectioner(self,other):
-        if self.hit_hat()>other.hit_hat():
-            return "{} have more expencive car than {}".format(self.name,other.name)
-        elif self.hit_hat()< other.hit_hat():
-            return "{} have less expencive car than {}".format(self.name,other.name)
-        else:
-            return "{} and {} have same cars".format(self.name,other.name)
-
-
-
     def __str__(self):
-        return "Collectioner(My name is %r,and i have %r garagers,here my register id %r)" % (self.name, self.garages, self.register_id)
+        return "Collectioner(My name is %r,and i have %r garagers,here my register id %r)" % (
+        self.name, self.garages, self.register_id)
 
     def __repr__(self):
-        return "Collectioner(My name is %r,and i have %r garagers,here my register id %r)" % (self.name, self.garages, self.register_id)
+        return "Collectioner(My name is %r,and i have %r garagers,here my register id %r)" % (
+        self.name, self.garages, self.register_id)
 
-#__TEST__ME__
+
+# __TEST__ME__
 
 car_1 = Car(20000, random.choice(CARS_TYPES), random.choice(CARS_PRODUCER), 17000)
 car_2 = Car(3248, random.choice(CARS_TYPES), random.choice(CARS_PRODUCER), 23443)
@@ -221,39 +214,39 @@ car_3 = Car(890, random.choice(CARS_TYPES), random.choice(CARS_PRODUCER), 23423)
 car_4 = Car(9078, random.choice(CARS_TYPES), random.choice(CARS_PRODUCER), 5675)
 car_5 = Car(8908, random.choice(CARS_TYPES), random.choice(CARS_PRODUCER), 89076)
 
-garage_1 = Garage([],random.choice(TOWNS),3)
-garage_2 = Garage([car_1],random.choice(TOWNS),7)
-garage_3 = Garage([car_2,car_3],random.choice(TOWNS),8)
-garage_4 = Garage([car_3,car_4,car_5],random.choice(TOWNS),1)
+garage_1 = Garage([], random.choice(TOWNS), 3)
+garage_2 = Garage([car_1], random.choice(TOWNS), 7)
+garage_3 = Garage([car_2, car_3], random.choice(TOWNS), 8)
+garage_4 = Garage([car_3, car_4, car_5], random.choice(TOWNS), 1)
 
 collectionar_1 = Collectioner('Jshua')
 collectionar_2 = Collectioner('Myhamed')
 
-#___CAR____
+# ___CAR____
 
 print(car_1)
 print(car_2)
 print(car_3.change_number())
 print(car_3.compair_cars_by_price(car_4))
 
-#___GARAGE___
+# ___GARAGE___
 
 print(garage_1)
 print(garage_1.add(car_4))
 print(garage_3.remove(car_2))
 print(garage_4.hit_hap())
 
-#___COLLECTIONARE___
+# ___COLLECTIONARE___
 collectionar_1.garages = [garage_1]
 collectionar_2.garages = [garage_4]
 print(collectionar_1)
 print(collectionar_1.hit_hat())
 print(collectionar_2.garages_count())
-print(collectionar_1.add_car(car_1,garage_2))
+print(collectionar_1.add_car(car_1, garage_2))
 print(collectionar_1)
 print(collectionar_1.hit_hat())
 
-print(collectionar_1.add_car(car_2,garage_1))
+print(collectionar_1.add_car(car_2, garage_1))
 print(collectionar_1)
 print(collectionar_1.hit_hat())
 print(collectionar_1.compare_collectioner(collectionar_2))
